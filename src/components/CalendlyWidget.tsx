913a4dbc5a9c62e0e1022cbb60d23b2ced2263d5
Saad <saadateeq@vinncorp.com>
2025-12-15 13:15:20 +0500

    calendly set

--- src/components/CalendlyWidget.tsx
+++ src/components/CalendlyWidget.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 declare global {
   interface Window {
@@ -7,7 +8,26 @@ declare global {
 }
 
 const CalendlyWidget = () => {
+  const location = useLocation();
+
   useEffect(() => {
+    // Only show Calendly widget on homepage
+    if (location.pathname !== '/') {
+      // Clean up any existing Calendly widgets
+      const calendlyBadge = document.querySelector('.calendly-badge-widget');
+      if (calendlyBadge) {
+        calendlyBadge.remove();
+      }
+      
+      const calendlyScript = document.querySelector('script[src="https://assets.calendly.com/assets/external/widget.js"]');
+      const calendlyStyle = document.querySelector('link[href="https://assets.calendly.com/assets/external/widget.css"]');
+      
+      if (calendlyScript) calendlyScript.remove();
+      if (calendlyStyle) calendlyStyle.remove();
+      
+      return;
+    }
+
     // Load Calendly CSS
     const link = document.createElement('link');
     link.href = 'https://assets.calendly.com/assets/external/widget.css';
@@ -47,8 +67,17 @@ const CalendlyWidget = () => {
       
       if (calendlyScript) calendlyScript.remove();
       if (calendlyStyle) calendlyStyle.remove();
+      
+      // Destroy Calendly instance if it exists
+      if (window.Calendly && window.Calendly.destroyBadgeWidget) {
+        try {
+          window.Calendly.destroyBadgeWidget();
+        } catch (e) {
+          console.log('Calendly cleanup:', e);
+        }
+      }
     };
-  }, []);
+  }, [location.pathname]);
 
   return null;
 };
